32ac474378e05d4482cf7196917e77022643d8b9
hoandevv <dohoan25112005@gmail.com>
2026-05-24 21:24:44 +0700

    initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/main/java/com/hoandev/pinedrink/PineDrinkApplication.java
@@ -0,0 +1,13 @@
+package com.hoandev.pinedrink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PineDrinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PineDrinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoandev/pinedrink/PineDrinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoandev.pinedrink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PineDrinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
